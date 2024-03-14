e299d12307a03b0e770196efdca2cb27eb55ec8e
SeeingForward <62649685+SeeingForward@users.noreply.github.com>
2024-03-14 16:31:21 +0100

    Remove unused remnants from DebuggerConsoleController.cpp
    Co-authored-by: MSgitStudent <74267120+MSgitStudent@users.noreply.github.com>

--- src/platform/qt/DebuggerGUIController.h
+++ src/platform/qt/DebuggerGUIController.h
@@ -24,33 +24,8 @@ Q_OBJECT
 public:
 	DebuggerGUIController(QObject* parent = nullptr);
 
-	QStringList history() const { return m_history; }
 	struct ARMRegisterFile* getGbaRegisters();
 	void DebuggerGUIController::setGbaRegister(uint8_t regId, uint32_t value);
-
-signals:
-	void log(const QString&);
-	void lineAppend(const QString&);
-
-public slots:
-	void enterLine(const QString&);
-	virtual void detach() override;
-	void historyLoad();
-	void historySave();
-
-protected:
-	virtual void attachInternal() override;
-
-private:
-	static void printf(struct CLIDebuggerBackend* be, const char* fmt, ...);
-	static void init(struct CLIDebuggerBackend* be);
-	static void deinit(struct CLIDebuggerBackend* be);
-	static int poll(struct CLIDebuggerBackend* be, int32_t timeoutMs);
-	static const char* readLine(struct CLIDebuggerBackend* be, size_t* len);
-	static void lineAppend(struct CLIDebuggerBackend* be, const char* line);
-	static const char* historyLast(struct CLIDebuggerBackend* be, size_t* len);
-	static void historyAppend(struct CLIDebuggerBackend* be, const char* line);
-	static void interrupt(struct CLIDebuggerBackend* be);
 
 
 	CLIDebugger m_cliDebugger{};
